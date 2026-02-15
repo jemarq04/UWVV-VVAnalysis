92bddf581f02041620a06939099c4883131ba62a
Justin Marquez <justin.marquez@wisc.edu>
2026-02-15 10:30:25 -0600

    chore: throw exception if analysis is not found

--- python/helpers.py
+++ python/helpers.py
@@ -9,6 +9,8 @@ def get_channels(analysis: str) -> list:
     channels = []
     if analysis == "ZZ4l":
         channels = ["eeee", "eemm", "mmmm"]
+    else:
+        raise ValueError(f"no channels found for analysis {analysis}")
     return channels
 
 
