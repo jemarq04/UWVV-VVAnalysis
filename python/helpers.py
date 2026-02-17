ba91a011df636bc36f03aadcc4ed5d752c31a123
Justin Marquez <justin.marquez@wisc.edu>
2026-02-17 08:48:21 -0600

    refactor: remove note comment

--- python/helpers.py
+++ python/helpers.py
@@ -1,7 +1,6 @@
 import os
 import json
 
-# NOTE: maybe get some sort of recipe/makefile that writes down the current directory for easy lookup in these scripts.
 BASE_DIR = os.environ["CMSSW_BASE"] + "/src/UWVV/VVAnalysis"
 JSON_DIR = os.path.join(BASE_DIR, "json")
 
