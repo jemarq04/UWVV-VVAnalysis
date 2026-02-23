6c7a72f999cb771b72db392b4928d76b9fe4cc32
Justin Marquez <justin.marquez@wisc.edu>
2026-02-23 11:22:56 -0600

    feat: add initial merging script and some helper functions, tested with ZPlusLFakeRateSelector

---
+++ python/mergetools.py
@@ -0,0 +1,141 @@
+import argparse
+import os
+import subprocess
+
+import ROOT
+
+
+def merge(args: argparse.Namespace):
+    """Merge input files and fill histograms to one output file.
+
+    Parameters
+    ----------
+    args : argparse.Namespace
+        A dict-like object parsed from the command-line containing information about
+        the job. Contains analysis, year, outfile, etc. Check merge.py to see
+        full list.
+
+    """
+    # Merging input files and saving histograms for each channel
+    tempfiles = []
+    for channel in get_channels(args.analysis):
+        if args.verbose:
+            print(f"{channel}:")
+            print("  Adding files...")
+
+        # Initialize tree
+        tree = ROOT.TChain(f"{channel}/ntuple")
+        for infile in args.infiles:
+            tree.Add(infile)
+
+        # Get selector to process events
+        outfile = args.outfile.replace(".root", f"_{channel}.root")
+        selector = get_selector(args.analysis, channel, args.sample, outfile)
+
+        # Apply selector
+        if args.verbose:
+            print("  Applying selector...")
+        tree.Process(selector)
+
+        # Track temporary output file
+        if args.verbose:
+            print(f"  Written to temporary file: {outfile}")
+        tempfiles.append(outfile)
+
+    # Combine each of the channel files to one file for the sample.
+    if args.verbose:
+        print(f"\nCombining temporary files to {args.outfile}")
+    status = subprocess.call(["hadd", "-f", args.outfile] + tempfiles)
+    if status == 0:
+        for tempfile in tempfiles:
+            os.remove(tempfile)
+
+
+def get_channels(analysis: str) -> list:
+    """Determine list of channels to process/merge for a given analysis.
+
+    Note that this is not the same function used for skimming. This is split
+    because the channels may differ between skimming and merging. For example,
+    ZZ4l skims [eeee, eemm, mmmm], but when it is processed eemm is split into
+    eemm and mmee, depending on which lepton pair is the primary Z candidate.
+
+    Parameters
+    ----------
+    analysis : str
+        The analysis with the desired channels.
+
+    Returns
+    -------
+    list of str
+        The list of channels for a given analysis.
+
+    """
+    channels = []
+    if analysis == "ZZ4l":
+        channels = ["eeee", "eemm", "mmee", "mmmm"]
+    elif analysis == "ZplusL":
+        channels = ["eee", "eem", "emm", "mmm"]
+    else:
+        raise NotImplementedError(f"no channels found for analysis {analysis}")
+    return channels
+
+
+def get_selector(analysis: str, channel: str, sample: str, outfile: str) -> ROOT.TSelector:
+    """Get selector appropriate for the given analysis to fill histograms.
+
+    Parameters
+    ----------
+    analysis : str
+        The analysis to check for a selector (e.g. ZZ4l).
+    channel : str
+        The channel to be processed (e.g. eeee or eemm).
+    sample : str
+        The sample name to be processed (e.g. qqZZ or ggHZZ_preBPix)
+    outfile : str
+        The name of the output file path to pass to the selector
+
+    Returns
+    -------
+    ROOT.TSelector
+        A TSelector object loaded with the appropriate inputs depending on
+        the given analysis and channel.
+
+    """
+    # Build counts of objects and the list of object names
+    # i.e. {"e": 2, "m": 2} -> ["e1", "e2", "m1", "m2"]
+    # (These are built slightly differently than in build_cutstring())
+    object_counts = {}
+    for obj in channel:
+        if obj in object_counts:
+            object_counts[obj] += 1
+        else:
+            object_counts[obj] = 1
+
+    object_names = []
+    for obj, count in object_counts.items():
+        object_names += [f"{obj}{i}" if count != 1 else obj for i in range(1, count + 1)]
+
+    # Build selector depending on analysis
+    if analysis == "ZplusL":
+        selector = ROOT.ZplusLFakeRateSelector()
+        inputs = ROOT.TList()
+        inputs.Add(ROOT.TNamed("name", sample))
+        inputs.Add(ROOT.TNamed("channel", channel))
+        inputs.Add(ROOT.TNamed("output", outfile))
+        inputs.Add(ROOT.TNamed("l1Tight", f"{object_names[0]}ZZTightID"))
+        inputs.Add(ROOT.TNamed("l2Tight", f"{object_names[1]}ZZTightID"))
+        inputs.Add(ROOT.TNamed("l3Tight", f"{object_names[2]}ZZTightID"))
+        inputs.Add(ROOT.TNamed("l1Iso", f"{object_names[0]}ZZIsoPass"))
+        inputs.Add(ROOT.TNamed("l2Iso", f"{object_names[1]}ZZIsoPass"))
+        inputs.Add(ROOT.TNamed("l3Iso", f"{object_names[2]}ZZIsoPass"))
+        inputs.Add(ROOT.TNamed("l3Pt", f"{object_names[2]}Pt"))
+        inputs.Add(ROOT.TNamed("l3Eta", f"{object_names[2]}Eta"))
+        inputs.Add(ROOT.TNamed("l3MtToMET", f"{object_names[2]}MtToMET"))
+        for obj, count in object_counts.items():
+            if count >= 2:
+                inputs.Add(ROOT.TNamed("Z1Mass", f"{obj}1_{obj}2_Mass"))
+        selector.SetInputList(inputs)
+    else:
+        raise NotImplementedError(f"no selector available for analysis: {analysis}")
+
+    return selector
---
+++ scripts/merge.py
@@ -0,0 +1,69 @@
+#!/usr/bin/env python3
+
+import argparse
+import json
+import os
+
+from UWVV.VVAnalysis import helpers, mergetools
+
+
+def main():
+    """Merge input files and fill histograms to one output file."""
+    parser = argparse.ArgumentParser(description=main.__doc__, formatter_class=helpers.CustomHelpFormatter)
+    parser.add_argument("-a", "--analysis", default="ZZ4l", help="name of analysis")
+    parser.add_argument("-y", "--year", default="2022", help="year for analysis")
+    parser.add_argument(
+        "-o", "--outfile", default=argparse.SUPPRESS, help="output file (default: Hists-<ANALYSIS>_<SAMPLE>.root)"
+    )
+    parser.add_argument("-v", "--verbose", action="store_true", help="print during skimming")
+    parser.add_argument(
+        "--skimmed-json",
+        default=argparse.SUPPRESS,
+        help="skimmed ntuple JSON (default: json/<ANALYSIS>/<YEAR>/skimmed.json)",
+    )
+    parser.add_argument("-i", "--infiles", nargs="+", help="input file(s)")
+    parser.add_argument(
+        "-s",
+        "--sample",
+        default=argparse.SUPPRESS,
+        required=True,
+        help="name of sample to be processed (e.g. qqZZ or data_MuonEG_Run2022C)",
+    )
+    args = parser.parse_args()
+
+    # Handle defaults
+    if "outfile" not in args:
+        args.outfile = f"Hists-{args.analysis}_{args.sample}.root"
+    if "skimmed_json" not in args:
+        args.skimmed_json = None
+
+    # Error checking
+    if not os.path.isdir(os.path.join(helpers.JSON_DIR, args.analysis)):
+        parser.error(f"invalid analysis: {args.analysis}")
+    if not os.path.isdir(os.path.join(helpers.JSON_DIR, args.analysis, args.year)):
+        parser.error(f"invalid year for analysis {args.analysis}: {args.year}")
+    if args.skimmed_json is not None and not os.path.isfile(args.skimmed_json):
+        parser.error(f"invalid skimmed JSON: {args.skimmed_json}")
+
+    # Loading infiles, if needed
+    if args.infiles is None:
+        # Load JSON information
+        if args.skimmed_json is not None:
+            with open(args.skimmed_dir) as infile:
+                skimmed = json.load(infile)
+        else:
+            skimmed = helpers.load_json(args.analysis, args.year, "skimmed.json")
+
+        # Error checking
+        if args.sample not in skimmed:
+            parser.error(f"invalid sample: {args.sample}")
+
+        # Read input files from JSON
+        args.infiles = skimmed[args.sample]
+
+    # Merge
+    mergetools.merge(args)
+
+
+if __name__ == "__main__":
+    main()
