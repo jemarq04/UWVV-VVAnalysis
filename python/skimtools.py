1fbfa5cd4246848f2441f2cdcde9a83d7b0f755a
Justin Marquez <justin.marquez@wisc.edu>
2026-03-12 14:12:18 -0500

    fix: revert previous commit and omit guard. if meta tree is empty, it should be investigated

--- python/skimtools.py
+++ python/skimtools.py
@@ -93,8 +93,7 @@ def skim(args: argparse.Namespace, cutinfo: dict, aliases: dict, triggers: dict)
         subdir = outfile.mkdir("metaInfo")
         subdir.cd()
         tree_copy = tree.CopyTree("")
-        if tree_copy:
-            tree_copy.Write()
+        tree_copy.Write()
 
     if args.verbose:
         print(f"Written to {args.outfile}")
